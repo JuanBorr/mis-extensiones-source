72ec8f207d3ec6eba8773521a5319eb97150aec2
JuanBorr <104606864+JuanBorr@users.noreply.github.com>
2026-05-22 15:34:44 -0500

    Create OlympusBiblioteca.kt

---
+++ src/es/olympusbiblioteca/src/es/olympusbiblioteca/src/eu/kanade/tachiyomi/extension/es/olympusbiblioteca/OlympusBiblioteca.kt
@@ -0,0 +1,9 @@
+package eu.kanade.tachiyomi.extension.es.olympusbiblioteca
+
+import eu.kanade.tachiyomi.multisrc.madara.Madara
+
+class OlympusBiblioteca : Madara(
+    "Olympus Biblioteca",
+    "https://olympusbiblioteca.com",
+    "es"
+)
